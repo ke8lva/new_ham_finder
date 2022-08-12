edfc1d93b7f24a04ffd725c3722f0da0a590b733
ke8lva <106828111+ke8lva@users.noreply.github.com>
2022-08-12 09:57:27 -0400

    Update newhams_weekly.py
    working directory added

--- newhams_weekly.py
+++ newhams_weekly.py
@@ -23,7 +23,7 @@ port = 587
 smtp_server = 'smtp.mail.server'
 sender_email = 'smtp@domain.com'
 password = 'yourpassword'
-
+working_directory = 'path/to/your/working/directory'
 
 """
 No changes need to be made below here.
@@ -45,7 +45,7 @@ url = "https://data.fcc.gov/download/pub/uls/complete/l_amat.zip"
 filename = url.split('/')[-1]
 response = requests.get(url)
 zipfile = zipfile.ZipFile(BytesIO(response.content))
-zipfile.extractall()
+zipfile.extractall(working_directory)
 response.close()
 #Send email function
 def email(address):
@@ -65,7 +65,7 @@ def email(address):
         server.login(sender_email, password)
         server.sendmail(sender_email, address, msg.as_string())
 #Build desired date range
-with open('HS.dat', 'r') as hs_data: 
+with open(f'{working_directory}/HS.dat', 'r') as hs_data: 
     temp_list = []
     hs_reader = csv.reader(hs_data, delimiter='|')
     for i in hs_reader:
@@ -81,7 +81,7 @@ with open('HS.dat', 'r') as hs_data:
         else:
             continue        
 #Get desired rows from "EN" file
-with open('EN.dat', 'r') as en_data:
+with open(f'{working_directory}/EN.dat', 'r') as en_data:
     en_reader = csv.reader(en_data, delimiter='|')
     for row in en_reader:
         if row[18][0:5] in zip_list:
@@ -90,7 +90,7 @@ with open('EN.dat', 'r') as en_data:
             continue
 #Get data from AM file to see if they are a ham that upgraded and recieved a 
 # new systematic callsign and that it is not a club callsign.
-with open('AM.dat', 'r') as am_data:
+with open(f'{working_directory}/AM.dat', 'r') as am_data:
     am_reader = csv.reader(am_data, delimiter='|')
     for row in am_reader:
         for i in newCall_list:
